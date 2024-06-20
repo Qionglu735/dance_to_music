8a66dfb7fb8a20a9a6cda97ea89c9df16ba4d5df
Qionglu735 <Qionglu735@gmail.com>
2024-06-21 03:30:30 +0900

    pitch detect (not working very well, comment out)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import aubio
 import asyncio
 import importlib
 import logging
@@ -16,10 +17,9 @@ Client = buttplug.Client
 ProtocolSpec = buttplug.ProtocolSpec
 WebsocketConnector = buttplug.WebsocketConnector
 
-threshold = 0.4
-threshold_adj = 0.6
-norm_size = 100
-norm_size_adj = 8
+threshold_adj = 0.08
+sample_size = 100
+sample_size_adj = 2
 
 
 class SoundThread(threading.Thread):
@@ -27,9 +27,14 @@ class SoundThread(threading.Thread):
         super(SoundThread, self).__init__()
         self._stop_event = threading.Event()
         self.volume_norm = 0
+        self.pitch_o = aubio.pitch("default", buf_size=512, hop_size=512, samplerate=44100)
+        self.pitch_o.set_unit("midi")
+        self.pitch_o.set_tolerance(0.8)
+        self.pitch = 0
 
     def print_sound(self, indata, outdata, frames, time, status):
         self.volume_norm = np.linalg.norm(indata)
+        self.pitch = self.pitch_o(indata[:, 0])[0]
         # print(f"{volume_norm}")
 
     def run(self):
@@ -65,13 +70,12 @@ async def main():
     global sound_thread
     sound_thread.start()
 
-    global threshold
-
-    norm_list = list()
+    volume_list = list()
+    pitch_list = list()
     while sound_thread.is_alive():
         try:
             client = Client("Music Vibrator", ProtocolSpec.v3)
-            connector = WebsocketConnector("ws://127.0.0.1:12345", logger=client.logger)
+            connector = WebsocketConnector("ws://127.0.0.1:22345", logger=client.logger)
             await client.connect(connector)
 
         except Exception:
@@ -114,26 +118,80 @@ async def main():
             await client.devices[device_index].actuators[0].command(i[0])
             await asyncio.sleep(i[1])
         while sound_thread.is_alive():
-            volume_norm = sound_thread.volume_norm
+            volume = sound_thread.volume_norm
+            pitch = sound_thread.pitch
 
-            if len(norm_list) >= norm_size * norm_size_adj:
-                norm_list = norm_list[len(norm_list) - norm_size * norm_size_adj + 1:]
-            if volume_norm >= 0.1:
-                norm_list.append(volume_norm)
-            if len(norm_list) == 0:
-                # print("len(norm_list) == 0 continue")
+            if volume >= 0.01:
+                volume_list.append(volume)
+            if len(volume_list) > sample_size * sample_size_adj:
+                volume_list = volume_list[len(volume_list) - sample_size * sample_size_adj:]
+            if len(volume_list) == 0:
+                # print("len(volume_list) == 0 continue")
                 continue
 
-            threshold = max(0.1, round(sum(norm_list) / len(norm_list) * 2 * threshold_adj, 2))
-            vibrate = float(1 - 1 / (max(volume_norm - threshold, 0) + 1))
+            if volume >= 0.01:
+                pitch_list.append(pitch)
+            if len(pitch_list) > sample_size * sample_size_adj:
+                pitch_list = pitch_list[len(pitch_list) - sample_size * sample_size_adj:]
+
+            # volume_threshold = max(0.01, round(sum(volume_list) / len(volume_list) * 2 * threshold_adj, 2))
+            # average of volume
+            volume_avg = sum(volume_list) / len(volume_list)
+            volume_avg = max(0.01, round(volume_avg, 2))
+            # average of volume * adj
+            volume_avg_adj = sum(volume_list) / len(volume_list) * (1 + threshold_adj)
+            volume_avg_adj = max(0.01, round(volume_avg_adj, 2))
+            # # distance between min and max value * adj
+            # volume_dis_adj = min(volume_list) + (max(volume_list) - min(volume_list)) * (0.5 + threshold_adj)
+            # volume_dis_adj = max(0.01, round(volume_dis_adj, 2))
+
+            volume_vibrate = float(1 - 1 / (max(volume - volume_avg_adj, 0) + 1))
+
+            # # average of pitch
+            # pitch_avg = sum(pitch_list) / len(pitch_list)
+            # pitch_avg = max(0.01, round(pitch_avg, 2))
+            # # average of pitch * adj
+            # pitch_avg_adj = sum(pitch_list) / len(pitch_list) * (1 + threshold_adj)
+            # pitch_avg_adj = max(0.01, round(pitch_avg_adj, 2))
+            # # distance between min and max value * adj
+            # pitch_dis_adj = min(pitch_list) + (max(pitch_list) - min(pitch_list)) * (0.5 + threshold_adj)
+            # pitch_dis_adj = max(0.01, round(pitch_dis_adj, 2))
+            #
+            # pitch_vibrate = float(1 - 1 / (max(pitch - pitch_avg_adj, 0) + 1))
+
+            vibrate = max([
+                volume_vibrate,
+                # pitch_vibrate,
+            ])
+
+            def bar_displayer(value, avg, avg_adj, value_list):
+                bar_size = 70
+                bar_low_end = 0
+                bar_base = max(1, max(value_list) - bar_low_end)
+                bar = int(math.floor((value - bar_low_end) / bar_base * bar_size))
+                avg_bar = int(math.floor((avg - bar_low_end) / bar_base * bar_size))
+                avg_adj_bar = int(math.floor((avg_adj - bar_low_end) / bar_base * bar_size))
+                bar_display = list(f"{'=' * bar }{' ' * (bar_size - bar)}")
+                if avg_bar < len(bar_display):
+                    bar_display[avg_bar] = "$"  # avg
+                if avg_adj_bar < len(bar_display):
+                    if bar_display[avg_adj_bar] == "=":
+                        bar_display[avg_adj_bar] = "!"  # avg * adj (active)
+                    else:
+                        bar_display[avg_adj_bar] = "|"  # avg * adj
+                return "".join(bar_display)
+
+            display = bar_displayer(volume, volume_avg, volume_avg_adj, volume_list)
+            # display = bar_displayer(pitch, pitch_avg, pitch_avg_adj, pitch_list)
 
             sys.stdout.write(
                 f"\r"
-                f"Volume: {round(float(volume_norm), 2)}/{threshold}({threshold_adj}) "
+                f"[{display}] "
+                f"Volume: {round(float(volume), 2)}/{volume_avg} ({threshold_adj}) "
+                # f"Pitch: {round(float(pitch), 2)}/{pitch_avg}/{pitch_dis_adj} ({threshold_adj}) "
                 f"Vibrate: {round(vibrate, 2)} "
-                f"Sample: {len(norm_list)}/{norm_size * norm_size_adj} "
-                # f"Vibrate: {round((volume_norm - threshold) / (max(norm_list) + 0.01 - threshold), 2)} "
-                # f"{round(sum(norm_list), 2)}/{len(norm_list)}={round(sum(norm_list) / len(norm_list), 2)}"
+                f"Sample: {len(volume_list)}/{sample_size * sample_size_adj} "
+                # f"Sample: {len(pitch_list)}/{sample_size * sample_size_adj} "
             )
             try:
                 await client.devices[device_index].actuators[0].command(min(1.0, vibrate))
@@ -150,7 +208,7 @@ async def main():
 
 def keyboard_handler(event):
     global threshold_adj
-    global norm_size_adj
+    global sample_size_adj
     if event.name in ["up", "w"]:
         threshold_adj = min(2.0, round(threshold_adj + 0.01, 2))
         # print(threshold)
@@ -158,10 +216,10 @@ def keyboard_handler(event):
         threshold_adj = max(-2.0, round(threshold_adj - 0.01, 2))
         # print(threshold)
     elif event.name in ["left", "a"]:
-        norm_size_adj = max(1, math.floor(norm_size_adj / 2))
+        sample_size_adj = max(1, math.floor(sample_size_adj / 2))
         # print(threshold)
     elif event.name in ["right", "d"]:
-        norm_size_adj = norm_size_adj * 2
+        sample_size_adj = sample_size_adj * 2
         # print(threshold)
     elif event.name in ["esc"]:
         global sound_thread
